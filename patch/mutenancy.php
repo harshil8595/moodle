bc34a8e1a3cdddee806a091b6079d7009abc5696
Petr Skoda <commits@skodak.org>
2025-04-24 14:03:45 +0200

    release mu-4.5.4-06

--- patch/mutenancy.php
+++ patch/mutenancy.php
@@ -12,5 +12,5 @@ return [
     'description' => 'Patches required by tool_mutenancy plugin which implements multi-tenancy support in Moodle™',
     'url' => 'https://github.com/mutms/moodle/tree/patch/mutenancy/MOODLE_405_STABLE',
     'maintainer' => 'Petr Skoda',
-    'release' => 'mu-4.5.4-05',
+    'release' => 'mu-4.5.4-06',
 ];
